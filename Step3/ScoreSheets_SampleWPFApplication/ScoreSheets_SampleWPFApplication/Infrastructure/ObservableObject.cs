256030e5dd2b637323c3f24bf2eb92de3ba53bdf
norikois <33947244+norikois@users.noreply.github.com>
2023-01-26 17:32:14 +0900

    Step 3 完了時のアプリケーション
    Step 3 完了時のアプリケーション

---
+++ Step3/ScoreSheets_SampleWPFApplication/ScoreSheets_SampleWPFApplication/Infrastructure/ObservableObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ScoreSheets_SampleWPFApplication.Infrastructure
+{
+#nullable enable
+    internal abstract class ObservableObject : INotifyPropertyChanged
+    {
+        protected ObservableObject()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] String? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+#nullable disable
+}
