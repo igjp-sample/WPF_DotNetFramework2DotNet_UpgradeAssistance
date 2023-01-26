ac222f72c0fe45809c57f0315e760f28055e102c
norikois <33947244+norikois@users.noreply.github.com>
2023-01-26 14:51:18 +0900

    Step 1 完了時のアプリケーション
    Step 1 完了時のアプリケーション

---
+++ Step1/ScoreSheets_SampleWPFApplication/ScoreSheets_SampleWPFApplication/Model/SubjectAndFrequencyData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ScoreSheets_SampleWPFApplication.Model
+{
+    internal class SubjectAndFrequencyData
+    {
+        public String Subject { get; set; }
+        public List<Frequency> Frequencies { get; set; }
+
+        public SubjectAndFrequencyData()
+        {
+        }
+    }
+}
